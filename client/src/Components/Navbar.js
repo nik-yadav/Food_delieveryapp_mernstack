b9eca101c13905a61010c249087563b1fc4c28b5
Nikhil Yadav <nikhil.yadav.a12@gmail.com>
2024-12-19 01:52:22 +0530

    updated navbar and reducer

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -2,13 +2,12 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-
   const navigate = useNavigate();
 
   const handleLogout = () => {
     localStorage.clear();
-    navigate("/login")
-  }
+    navigate("/login");
+  };
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-success">
@@ -30,26 +29,45 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav me-auto mb-2">
               <li className="nav-item">
-                <Link className="nav-link active fs-5" aria-current="page" to="/">
+                <Link
+                  className="nav-link active fs-5"
+                  aria-current="page"
+                  to="/"
+                >
                   Home
                 </Link>
               </li>
-              {(localStorage.getItem("authToken")) ? 
+              {localStorage.getItem("authToken") ? (
                 <li className="nav-item">
-                  <Link className="nav-link active fs-5" aria-current="page" to="/myorder">
+                  <Link
+                    className="nav-link active fs-5"
+                    aria-current="page"
+                    to="/myorder"
+                  >
                     My Orders
                   </Link>
                 </li>
-                :""}
+              ) : (
+                ""
+              )}
             </ul>
 
-            {(localStorage.getItem("authToken")) ? 
-            <div>
-              <Link to={"/cart"} className="btn bg-white text-success mx-2"> My Cart </Link>
-              <div className="btn bg-white text-danger mx-2" onClick={handleLogout}> Logout </div>
-            </div>
-            :
-            <div className="d-flex">
+            {localStorage.getItem("authToken") ? (
+              <div>
+                <Link to={"/cart"} className="btn bg-white text-success mx-2">
+                  {" "}
+                  My Cart{" "}
+                </Link>
+                <div
+                  className="btn bg-white text-danger mx-2"
+                  onClick={handleLogout}
+                >
+                  {" "}
+                  Logout{" "}
+                </div>
+              </div>
+            ) : (
+              <div className="d-flex">
                 <Link className="btn bg-white text-success mx-1" to="/login">
                   Login
                 </Link>
@@ -57,8 +75,8 @@ const Navbar = () => {
                 <Link className="btn bg-white text-success mx-1" to="/signup">
                   SignUp
                 </Link>
-            </div>
-            }
+              </div>
+            )}
           </div>
         </div>
       </nav>
